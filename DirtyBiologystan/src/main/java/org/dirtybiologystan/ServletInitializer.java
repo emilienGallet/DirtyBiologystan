90bfe07601097534764ac83e269a9d9c26a6b91e
emilienGallet <emilienflorentclaude.gallet@gmail.com>
2021-11-04 17:29:47 +0100

    Quick fix des gi en gy de dirtybiologystan again

--- DirtyBiologystan/src/main/java/org/dirtybiologystan/ServletInitializer.java
+++ DirtyBiologystan/src/main/java/org/dirtybiologystan/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(DirtyBiologistanApplication.class);
+		return application.sources(DirtyBiologystanApplication.class);
 	}
 
 }
