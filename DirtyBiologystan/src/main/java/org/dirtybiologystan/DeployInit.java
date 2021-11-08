42674c5dff448c18b26e4578930b9db4b9b9e23a
emilienGallet <emilienflorentclaude.gallet@gmail.com>
2021-11-08 13:11:17 +0100

    Passage en mod dev et rajout du path /citoyenneté

--- DirtyBiologystan/src/main/java/org/dirtybiologystan/DeployInit.java
+++ DirtyBiologystan/src/main/java/org/dirtybiologystan/DeployInit.java
@@ -15,19 +15,19 @@ public class DeployInit {
 	///////////////////////////////////////////////////////
 	
 	//Debug mod 
-	/*
+	/**/
 	public static final String Default_Path = System.getProperty("user.dir");
 	public static final String PathDeploy = Default_Path+"/src/main/resources";//+"/ROOT/WEB-INF/classes";
 	public static final String PathImgDeploy = PathDeploy + "/static";//""
 	public static final String PathResourcesDeploy = "";
-	public static final String NomDeDomaine = "https://localhost:8443/";
-	*/
-	//Live Mod
+	public static final String NomDeDomaine = "http://localhost:8080/";
 	/**/
+	//Live Mod
+	/*
 	public static final String Default_Path = "";
 	public static final String PathDeploy = Default_Path+"";
 	public static final String PathImgDeploy = PathDeploy + "";//""
 	public static final String PathResourcesDeploy = "/dirtybiologystan";
 	public static final String NomDeDomaine = "https://www.dirtybiologystan.org/";//Déja pris a modifier
-	/**/
+	*/
 }
